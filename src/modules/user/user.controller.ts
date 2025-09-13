d0979874b807df844c29c16eb786c8de538a0964
obidyhasan <obidyhasan@gmail.com>
2025-09-13 09:57:34 +0600

    include user modules functionality

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,31 +1,45 @@
 import { Request, Response } from "express";
+import { UserService } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
   try {
+    const result = await UserService.createUser(req.body);
+    res.status(201).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 const getAllUsers = async (req: Request, res: Response) => {
   try {
+    const result = await UserService.getAllUsers();
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 const getUserById = async (req: Request, res: Response) => {
   try {
+    const result = await UserService.getUserById(Number(req.params.id));
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 const updateUserById = async (req: Request, res: Response) => {
   try {
+    const result = await UserService.updateUserById(
+      Number(req.params.id),
+      req.body
+    );
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 const deleteUserById = async (req: Request, res: Response) => {
   try {
+    const result = await UserService.deleteUserById(Number(req.params.id));
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,84 @@
-const createUser = () => {};
-const getAllUsers = () => {};
-const getUserById = () => {};
-const updateUserById = () => {};
-const deleteUserById = () => {};
+import { Prisma, User } from "@prisma/client";
+import { prisma } from "../../config/db";
+
+const createUser = async (payload: Prisma.UserCreateInput): Promise<User> => {
+  const user = await prisma.user.create({
+    data: payload,
+  });
+  return user;
+};
+const getAllUsers = async () => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      phone: true,
+      picture: true,
+      status: true,
+      isVerified: true,
+      Post: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return users;
+};
+const getUserById = async (id: number) => {
+  const user = await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      phone: true,
+      picture: true,
+      status: true,
+      isVerified: true,
+      Post: true,
+    },
+  });
+  return user;
+};
+const updateUserById = async (id: number, payload: Prisma.UserUpdateInput) => {
+  const result = await prisma.user.update({
+    where: { id },
+    data: payload,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      phone: true,
+      picture: true,
+      status: true,
+      isVerified: true,
+      Post: true,
+    },
+  });
+
+  return result;
+};
+const deleteUserById = async (id: number) => {
+  const result = await prisma.user.delete({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      phone: true,
+      picture: true,
+      status: true,
+      isVerified: true,
+      Post: true,
+    },
+  });
+  return result;
+};
 
 export const UserService = {
   createUser,
