6e131903083cd7c582fbfbb2418318711916a284
obidyhasan <obidyhasan@gmail.com>
2025-09-13 10:30:01 +0600

    include post modules functionality

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,31 +1,45 @@
 import { Request, Response } from "express";
+import { PostService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
   try {
+    const result = await PostService.createPost(req.body);
+    res.status(201).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 const getAllPosts = async (req: Request, res: Response) => {
   try {
+    const result = await PostService.getAllPosts();
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 const getPostById = async (req: Request, res: Response) => {
   try {
+    const result = await PostService.getPostById(Number(req.params.id));
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 const updatePostById = async (req: Request, res: Response) => {
   try {
+    const result = await PostService.updatePostById(
+      Number(req.params.id),
+      req.body
+    );
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 const deletePostById = async (req: Request, res: Response) => {
   try {
+    const result = await PostService.deletePostById(Number(req.params.id));
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error);
   }
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,8 +1,48 @@
-const createPost = () => {};
-const getAllPosts = () => {};
-const getPostById = () => {};
-const updatePostById = () => {};
-const deletePostById = () => {};
+import { Post, Prisma } from "@prisma/client";
+import { prisma } from "../../config/db";
+
+const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
+  const result = await prisma.post.create({
+    data: payload,
+  });
+  return result;
+};
+const getAllPosts = async () => {
+  const result = await prisma.post.findMany({
+    include: {
+      author: {
+        select: {
+          name: true,
+          email: true,
+          phone: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return result;
+};
+const getPostById = async (id: number) => {
+  const result = await prisma.post.findUnique({
+    where: { id },
+  });
+  return result;
+};
+const updatePostById = async (id: number, payload: Prisma.PostUpdateInput) => {
+  const result = await prisma.post.update({
+    where: { id },
+    data: payload,
+  });
+  return result;
+};
+const deletePostById = async (id: number) => {
+  const result = await prisma.post.delete({
+    where: { id },
+  });
+  return result;
+};
 
 export const PostService = {
   createPost,
